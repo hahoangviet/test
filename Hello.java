8a5fced00aa36257c8727422e16565076bbabfb0
hahoangviet <hahoangviet126@gmail.com>
2020-04-30 17:35:24 +0200

    merge test

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
     public static void greet(String name) {
-        System.out.println(greeting() + ", " + name);
+        System.out.println(greeting() + ", " + name + "!");
     }
     public static String greeting() {
         return "Hello";
