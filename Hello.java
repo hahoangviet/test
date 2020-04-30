20dd90ff75501c92ed7e5b4de84fced51b6e2e9c
hahoangviet <hahoangviet126@gmail.com>
2020-04-30 17:48:37 +0200

    mm

--- Hello.java
+++ Hello.java
@@ -3,6 +3,11 @@ public class Hello {
         System.out.println(greeting() + ", " + name + "!");
     }
     public static String greeting() {
-        return "Ciao, ";
+        return "Ciao ";
     }
+
+public static void main(String[] args) {
+	Hello.greet("Eve");
+}
+
 }
