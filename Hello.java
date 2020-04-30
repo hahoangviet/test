4a4a5314ea8fbd2a4f3b5ba5af362ba58af902dd
Nguyentruc Tran <56174522+i6213546@users.noreply.github.com>
2020-04-30 17:48:26 +0200

    Bob

--- Hello.java
+++ Hello.java
@@ -1,9 +1,9 @@
 public class Hello {
     public static void greet(String name) {
-        System.out.println(greeting() + ", " + name + "!");
+        System.out.println(greeting() + name);
     }
     public static String greeting() {
-        return "Ciao";
+        return "Hello, ";
     }
 public static void main(String[] args) {
 	greet("Eve");
